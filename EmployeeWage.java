c7d90be94dd55bac2bb90d083b878ce0e3317ee3
1598ashutoshm <ashutoshm1598@gmail.com>
2021-04-24 13:20:28 +0530

    Added: UC1 Functionality is added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,6 @@
 class EmployeeWage {
           public static void main(String[] args) {
                         System.out.println("Welcome Employees");
+                        System.out.println("Branch 1 Code is Added");
           }
 }
